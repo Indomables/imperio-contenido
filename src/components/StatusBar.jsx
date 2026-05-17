96983f44bf924b6b14d80f657abb46991a26634f
Indomables <herramientas@imperioindomable.com>
2026-05-17 18:46:30 +0200

    Security headers en netlify.toml (v0.48.2-α)

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -58,7 +58,7 @@ export default function StatusBar() {
     <div className="contenido-statusbar">
       <div className="l">
         <span>
-          <span className="led"></span> IMPERIO·CONTENIDO <b>v0.48.1</b>
+          <span className="led"></span> IMPERIO·CONTENIDO <b>v0.48.2</b>
         </span>
         {leftItems.map((item, idx) => (
           <StatusItem key={`l-${idx}`} item={item} />
